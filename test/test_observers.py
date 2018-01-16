42d71105bb0126a0bdf836356599789e121575f1
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-16 19:07:15 +0100

    better colors for observer plot

--- test/test_observers.py
+++ test/test_observers.py
@@ -8,9 +8,16 @@ import matplotlib.pyplot as plt
 def test_observers():
     lmbda, data = colorio.observers.cie_standard_observer_2()
 
-    plt.plot(lmbda, data[0])
-    plt.plot(lmbda, data[1])
-    plt.plot(lmbda, data[2])
+    # Choose the colors such that they roughly correspond to the color of the
+    # maximum response,
+    # data[0]: 595 nm
+    # data[1]: 557 nm
+    # data[2]: 445 nm
+    #
+    # TODO adjust this better
+    plt.plot(lmbda, data[0], color='#FFC400')
+    plt.plot(lmbda, data[1], color='#ABFF00')
+    plt.plot(lmbda, data[2], color='#0019FF')
 
     plt.xlabel('wavelength (nm)')
     plt.grid()
