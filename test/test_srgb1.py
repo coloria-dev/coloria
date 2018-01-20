ebe24d9f8faec420f1de0927193b63b1dc2e9b2e
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-20 12:16:08 +0100

    better test coverage

--- test/test_srgb1.py
+++ test/test_srgb1.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 #
+import numpy
+import pytest
+
 import colorio
+
+
+@pytest.mark.parametrize('vals', [
+    numpy.random.rand(3),
+    numpy.random.rand(3, 7),
+    ])
+def test_conversion(vals):
+    out = colorio.srgb1.to_srgb_linear(colorio.srgb1.from_srgb_linear(vals))
+    print(vals)
+    print(out)
+    assert numpy.all(abs(vals - out) < 1.0e-14)
+    return
 
 
 def test_gamut():
