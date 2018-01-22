5367da8a082d40da358bf593001a163d89acaf7b
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-22 23:50:55 +0100

    fix for xyy

--- colorio/xyy.py
+++ colorio/xyy.py
@@ -13,8 +13,8 @@ class XYY(object):
     def from_xyz100(self, xyz):
         sum_xyz = numpy.sum(xyz, axis=0)
         x, y, _ = xyz
-        return numpy.array([x/sum_xyz, y/sum_xyz, y])
+        return numpy.array([x/sum_xyz, y/sum_xyz, y/100])
 
     def to_xyz100(self, xyy):
         x, y, Y = xyy
-        return numpy.array([Y/y*x, Y, Y/y * (1-x-y)])
+        return 100 * numpy.array([Y/y*x, Y, Y/y * (1-x-y)])
