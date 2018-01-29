61b6d8d071d6e28b0d6d3430d7a753bc34058f6d
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-29 21:21:00 +0100

    version bump

--- colorio/__about__.py
+++ colorio/__about__.py
@@ -6,6 +6,6 @@ __email__ = 'nico.schloemer@gmail.com'
 __copyright__ = u'Copyright (c) 2018 {} <{}>'.format(__author__, __email__)
 __credits__ = []
 __license__ = 'License :: OSI Approved :: MIT License'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __maintainer__ = 'Nico Schlömer'
 __status__ = 'Development Status :: 1 - Planning'
