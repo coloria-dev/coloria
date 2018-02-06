de90f5b0b9bcfa7f7aed266a1ca68d2aa420e88a
Nico Schlömer <nico.schloemer@gmail.com>
2018-02-06 14:10:25 +0100

    more curve-fitting attempts

--- experiments/curve_fit.py
+++ experiments/curve_fit.py
@@ -1,22 +1,43 @@
 # -*- coding: utf-8 -*-
 #
+'''
+Curve-fit observer curves with sums of exp(-x**2).
+'''
 from __future__ import print_function
 
 import matplotlib.pyplot as plt
 import numpy
 import scipy.optimize
+import scipy.special
 
 import colorio
 
 
 def f(x, *p):
-    return p[0] * numpy.exp(-(x-p[1])**2 / p[2]**2)
+    return p[0]/numpy.sqrt(2*numpy.pi) * numpy.exp(-(x-p[1])**2 / (2*p[2]**2))
 
 
-def f2(x, *p):
+def f3(x, *p):
     return (
-        + p[0] * numpy.exp(-(x-p[1])**2 / p[2]**2)
-        + p[3] * numpy.exp(-(x-p[4])**2 / p[5]**2)
+        + p[0]/numpy.sqrt(2*numpy.pi) * numpy.exp(-(x-p[1])**2 / (2*p[2]**2))
+        + p[3]/numpy.sqrt(2*numpy.pi) * numpy.exp(-(x-p[4])**2 / (2*p[5]**2))
+        + p[6]/numpy.sqrt(2*numpy.pi) * numpy.exp(-(x-p[7])**2 / (2*p[8]**2))
+        )
+
+
+# With a skew <https://en.wikipedia.org/wiki/Skew_normal_distribution>
+def g(x, *p):
+    return (
+        p[0]
+        * 1/numpy.sqrt(2*numpy.pi) * numpy.exp(-(x-p[1])**2 / (2*p[2]**2))
+        * (1 + scipy.special.erf(p[3]*(x-p[1]) / 2 / p[2]))
+        )
+
+
+def pade(x, *p):
+    return (
+        (p[0] + p[1]*x + p[2]*x**2 + p[3]*x**3) /
+        (p[4] + p[5]*x + p[6]*x**2 + p[7]*x**3)
         )
 
 
@@ -31,14 +52,30 @@ def cv():
             f, lmbda, data[1], p0=[1, 0, 1],
             )
     print(popt1)
+
     popt2, _ = scipy.optimize.curve_fit(
-            f2, lmbda, data[1], p0=numpy.concatenate([popt1, [1, 0, 1]]),
+            g, lmbda, data[1], p0=[1, 0, 1, 0],
+            maxfev=10000
             )
     print(popt2)
 
-    plt.plot(lmbda, data[1])
+    popt3, _ = scipy.optimize.curve_fit(
+            pade, lmbda, data[1], p0=[1, 0, 0, 0, 1, 0, 0, 0],
+            maxfev=10000
+            )
+    print(popt3)
+
+    popt4, _ = scipy.optimize.curve_fit(
+            f3, lmbda, data[1], p0=[1, 0, 1, 1, 0, 1, 1, 0, 1],
+            maxfev=10000
+            )
+    print(popt4)
+
+    plt.plot(lmbda, data[1], '.')
     plt.plot(lmbda, f(lmbda, *popt1), ':')
-    plt.plot(lmbda, f2(lmbda, *popt2), '.')
+    # plt.plot(lmbda, g(lmbda, *popt2), ':')
+    # plt.plot(lmbda, pade(lmbda, *popt3), ':')
+    plt.plot(lmbda, f3(lmbda, *popt4), ':')
     plt.show()
     return
 
