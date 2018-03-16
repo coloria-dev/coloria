4b4091fdf649234c7685d6f16fc322ab0a62c10c
Nico Schlömer <nico.schloemer@gmail.com>
2018-03-16 15:17:13 +0100

    simplify new-cs

--- experiments/new-cs.py
+++ experiments/new-cs.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 #
+from __future__ import print_function
+
 import os
 
 import colorio
@@ -10,21 +12,96 @@ from scipy.optimize import leastsq, least_squares
 import yaml
 
 
-class New(object):
-    def __init__(self):
-        a = colorio.illuminants.whitepoints_cie1931['D65']
-        b = numpy.array([100.0, 0.0, 0.0])
-        # Get a matrix that maps a to b.
-        # <https://math.stackexchange.com/a/2672702/36678>
-        aa = a / numpy.sqrt(numpy.dot(a, a))
-        bb = b / numpy.sqrt(numpy.dot(b, b))
-        ab = aa + bb
-        self.M = 2 * numpy.outer(ab, ab) / numpy.dot(ab, ab) - numpy.eye(3)
-        self.M *= numpy.sqrt(numpy.dot(b, b)) / numpy.sqrt(numpy.dot(a, a))
-        return
+def evaluate_2d_polynomial(xy, alpha):
+    x, y = xy
+    out = 0.0
+    for a in alpha:
+        d = len(a)
+        for i in range(d):
+            out += a[i] * x**(d-i) * y**i
+    return out
 
-    def from_xyz100(self, xyz):
-        return numpy.dot(self.M, xyz)
+
+def create_triangle(alpha, degree):
+    return [
+        [alpha[d*(d+1)//2 + i] for i in range(d+1)]
+        for d in range(degree+1)
+        ]
+
+
+def transform(xy, alpha, num_coefficients, poly_degrees):
+    n = 0
+
+    alpha1 = numpy.concatenate([[0.0], alpha[n:n+num_coefficients[0]]])
+    a1 = create_triangle(alpha1, poly_degrees[0])
+    n += num_coefficients[0]
+
+    alpha2 = numpy.concatenate([[1.0], alpha[n:n+num_coefficients[1]]])
+    a2 = create_triangle(alpha2, poly_degrees[1])
+    n += num_coefficients[1]
+
+    beta1 = numpy.concatenate([[0.0], alpha[n:n+num_coefficients[2]]])
+    b1 = create_triangle(beta1, poly_degrees[2])
+    n += num_coefficients[2]
+
+    beta2 = numpy.concatenate([[1.0], alpha[n:n+num_coefficients[3]]])
+    b2 = create_triangle(beta2, poly_degrees[3])
+    n += num_coefficients[3]
+
+    return numpy.array([
+        evaluate_2d_polynomial(xy, a1) / evaluate_2d_polynomial(xy, a2),
+        evaluate_2d_polynomial(xy, b1) / evaluate_2d_polynomial(xy, b2),
+        ])
+
+
+def get_radii(alpha, num_coefficients, poly_degrees, centers, points):
+    A = []
+    B = []
+    for center, pts in zip(centers, points):
+        tcenter = transform(center, alpha, num_coefficients, poly_degrees)
+        X = (
+            transform(pts, alpha, num_coefficients, poly_degrees).T - tcenter
+            ).T
+
+        def f_ellipse(a_b_theta, x=X):
+            a, b, theta = a_b_theta
+            return (
+                + a**2 * (x[0] * numpy.cos(theta) + x[1] * numpy.sin(theta))**2
+                + b**2 * (x[0] * numpy.sin(theta) - x[1] * numpy.cos(theta))**2
+                - 1.0
+                )
+
+        def jac_ellipse(a_b_theta, x=X):
+            a, b, theta = a_b_theta
+            return numpy.array([
+                + 2*a * (x[0] * numpy.cos(theta) + x[1] * numpy.sin(theta))**2,
+                + 2*b * (x[0] * numpy.sin(theta) - x[1] * numpy.cos(theta))**2,
+                + a**2 * 2*(x[0] * numpy.cos(theta) + x[1] * numpy.sin(theta))
+                * (-x[0] * numpy.sin(theta) + x[1] * numpy.cos(theta))
+                + b**2 * 2*(x[0] * numpy.sin(theta) - x[1] * numpy.cos(theta))
+                * (x[0] * numpy.cos(theta) + x[1] * numpy.sin(theta)),
+                ]).T
+
+        (a, b, theta), _ = \
+            leastsq(f_ellipse, [1.0, 1.0, 0.0], Dfun=jac_ellipse)
+
+        A.append(1/a)
+        B.append(1/b)
+
+        # plt.plot(*X, 'x')
+        # ax = plt.gca()
+        # e = Ellipse(
+        #     xy=[0.0, 0.0],
+        #     width=2/a, height=2/b,
+        #     angle=theta / numpy.pi * 180
+        #     )
+        # ax.add_artist(e)
+        # e.set_alpha(0.5)
+        # e.set_facecolor('k')
+        # plt.show()
+
+    return numpy.concatenate([A, B])
+    # return numpy.log(ab / target_radius)
 
 
 def _main():
@@ -52,63 +129,14 @@ def _main():
             ).T)
 
 
-    # # plot the points
-    # for center, pts in zip(centers, points):
-    #     plt.plot(*center, 'x', color='k')
-    #     plt.plot(*pts, '.', color='k')
-    # plt.gca().set_aspect('equal')
-    # plt.show()
-
     centers = numpy.array(centers)
 
     target_radius = 2.0e-3
 
-    poly_degrees = [10, 10, 10, 10]
+    poly_degrees = [2, 2, 2, 2]
 
     # Subtract 1 for each polynomial since the constant coefficient is fixed.
     num_coefficients = [(d+1)*(d+2)//2 - 1 for d in poly_degrees]
-
-
-    def evaluate_2d_polynomial(xy, alpha):
-        x, y = xy
-        out = 0.0
-        for a in alpha:
-            d = len(a)
-            for i in range(d):
-                out += a[i] * x**(d-i) * y**i
-        return out
-
-
-    def create_triangle(alpha, degree):
-        return [
-            [alpha[d*(d+1)//2 + i] for i in range(d+1)]
-            for d in range(degree+1)
-            ]
-
-
-    def transform(xy, alpha):
-        n = 0
-
-        alpha1 = numpy.concatenate([[0.0], alpha[n:n+num_coefficients[0]]])
-        a1 = create_triangle(alpha1, poly_degrees[0])
-        n += num_coefficients[0]
-
-        alpha2 = numpy.concatenate([[1.0], alpha[n:n+num_coefficients[1]]])
-        a2 = create_triangle(alpha2, poly_degrees[1])
-        n += num_coefficients[1]
-
-        beta1 = numpy.concatenate([[0.0], alpha[n:n+num_coefficients[2]]])
-        b1 = create_triangle(beta1, poly_degrees[2])
-        n += num_coefficients[2]
-
-        beta2 = numpy.concatenate([[1.0], alpha[n:n+num_coefficients[3]]])
-        b2 = create_triangle(beta2, poly_degrees[3])
-        n += num_coefficients[3]
-
-        return numpy.array([
-            evaluate_2d_polynomial(xy, a1) / evaluate_2d_polynomial(xy, a2),
-            evaluate_2d_polynomial(xy, b1) / evaluate_2d_polynomial(xy, b2),
-            ])
 
 
     def f1(alpha):
@@ -133,64 +161,22 @@ def _main():
         # plt.show()
         return abs(dist - target_radius)
 
-    def get_radii(alpha):
-        A = []
-        B = []
-        for center, pts in zip(centers, points):
-            tcenter = transform(center, alpha)
-            X = (transform(pts, alpha).T - tcenter).T
-
-            def f_ellipse(a_b_theta, x=X):
-                a, b, theta = a_b_theta
-                return (
-                    + a**2 * (x[0] * numpy.cos(theta) + x[1] * numpy.sin(theta))**2
-                    + b**2 * (x[0] * numpy.sin(theta) - x[1] * numpy.cos(theta))**2
-                    - 1.0
-                    )
-
-            def jac_ellipse(a_b_theta, x=X):
-                a, b, theta = a_b_theta
-                return numpy.array([
-                    + 2*a * (x[0] * numpy.cos(theta) + x[1] * numpy.sin(theta))**2,
-                    + 2*b * (x[0] * numpy.sin(theta) - x[1] * numpy.cos(theta))**2,
-                    + a**2 * 2*(x[0] * numpy.cos(theta) + x[1] * numpy.sin(theta))
-                    * (-x[0] * numpy.sin(theta) + x[1] * numpy.cos(theta))
-                    + b**2 * 2*(x[0] * numpy.sin(theta) - x[1] * numpy.cos(theta))
-                    * (x[0] * numpy.cos(theta) + x[1] * numpy.sin(theta)),
-                    ]).T
-
-            (a, b, theta), _ = \
-                leastsq(f_ellipse, [1.0, 1.0, 0.0], Dfun=jac_ellipse)
-
-            A.append(1/a)
-            B.append(1/b)
-
-            # plt.plot(*X, 'x')
-            # ax = plt.gca()
-            # e = Ellipse(
-            #     xy=[0.0, 0.0],
-            #     width=2/a, height=2/b,
-            #     angle=theta / numpy.pi * 180
-            #     )
-            # ax.add_artist(e)
-            # e.set_alpha(0.5)
-            # e.set_facecolor('k')
-            # plt.show()
-
-        return numpy.concatenate([A, B])
-        # return numpy.log(ab / target_radius)
-
     def f2(alpha):
-        return get_radii(alpha) - target_radius
+        return (
+            get_radii(alpha, num_coefficients, poly_degrees, centers, points)
+            - target_radius
+            )
 
 
     # Create the identity function as initial guess
     total_num_parameters = numpy.sum(num_coefficients)
+    print('num parameters: {}'.format(total_num_parameters))
     coeff0 = numpy.zeros(total_num_parameters)
     i0 = 0
     coeff0[i0] = 1.0
     j0 = num_coefficients[0] + num_coefficients[1] + 1
     coeff0[j0] = 1.0
+    print('initial parameters:')
     print(coeff0)
     # out = leastsq(f, coeff0, full_output=True)
     # print(out)
@@ -201,10 +187,12 @@ def _main():
     # problems, but it needs more conditions than parameters.
     out = least_squares(f2, coeff0, method='trf')
     coeff1 = out.x
+    print()
+    print('optimal parameters:')
     print(coeff1)
 
-    radii0 = get_radii(coeff0)
-    radii1 = get_radii(coeff1)
+    radii0 = get_radii(coeff0, num_coefficients, poly_degrees, centers, points)
+    radii1 = get_radii(coeff1, num_coefficients, poly_degrees, centers, points)
     plt.plot(numpy.arange(len(radii0)), radii0, label='radii0')
     plt.plot(numpy.arange(len(radii1)), radii1, label='radii1')
     plt.legend()
@@ -218,7 +206,7 @@ def _main():
     plt.figure()
     colorio.plot_macadam(
         scaling=10,
-        xy_to_2d=lambda xy: transform(xy, coeff1),
+        xy_to_2d=lambda xy: transform(xy, coeff1, num_coefficients, poly_degrees),
         plot_standard_deviations=True
         )
     plt.show()
