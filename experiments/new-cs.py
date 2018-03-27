88139ecb310c0c88fef4caa7111388d16d967679
Nico Schlömer <nico.schloemer@gmail.com>
2018-03-27 14:26:45 +0200

    new-cs: first steps with jacobian

--- experiments/new-cs.py
+++ experiments/new-cs.py
@@ -11,6 +11,7 @@ from dolfin import (
     )
 import matplotlib.pyplot as plt
 import numpy
+from scipy.sparse.linalg import LinearOperator
 from scipy.optimize import leastsq, least_squares, minimize
 import yaml
 
@@ -310,7 +311,7 @@ class PiecewiseEllipse(object):
             corners=numpy.array([
                 [0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]
                 ]),
-            ref_steps=2
+            ref_steps=3
             )
 
         # https://bitbucket.org/fenics-project/dolfin/issues/845/initialize-mesh-from-vertices
@@ -328,6 +329,7 @@ class PiecewiseEllipse(object):
         self.ux = Function(self.V)
         self.uy = Function(self.V)
 
+        # self.alpha = self.u.vector().get_local()
         self.ax = self.ux.vector().get_local()
         self.ay = self.uy.vector().get_local()
         self.alpha = numpy.concatenate([self.ax, self.ay])
@@ -337,19 +339,60 @@ class PiecewiseEllipse(object):
         u = TrialFunction(self.V)
         v = TestFunction(self.V)
         self.L = assemble(dot(grad(u), grad(v)) * dx)
+
+        # The functions are locally linear, so we can cast the projection into a
+        # matrix. The matrix will be very sparse, but never mind that now; just
+        # use an array.
+        n = self.V.dim()
+        self.jacs = []
+        for k in range(n):
+            e = numpy.zeros(n)
+            e[k] = 1.0
+
+            u = Function(self.V)
+            u.vector().set_local(e)
+
+            j = project(grad(u), self.Vgrad)
+            self.jacs.append([j(x, y) for x, y in self.centers])
+
+        self.jacs = numpy.array(self.jacs).T
         return
 
     def get_q2_r2(self):
-        jx = project(grad(self.ux), self.Vgrad)
-        jy = project(grad(self.uy), self.Vgrad)
+        # Keep an eye on
+        # https://www.allanswered.com/post/eqkmm/matrix-from-project-evaluation-at-many-points-at-once/
+        # https://bitbucket.org/fenics-project/dolfin/issues/1011/evaluate-expressions-at-many-points-at
+        # for speeding this up.
+        # jx = project(grad(self.ux), self.Vgrad)
+        # jy = project(grad(self.uy), self.Vgrad)
+        # jac = numpy.array([[jx(x, y), jy(x, y)] for x, y in self.centers])
+        # jac = numpy.moveaxis(jac, 0, -1)
+        # assert numpy.all(abs(jj - jac) < 1.0e-15)
 
-        jac = numpy.array([[jx(x, y), jy(x, y)] for x, y in self.centers])
-        jac = numpy.moveaxis(jac, 0, -1)
+        # u = numpy.array([
+        #     self.ux.vector().get_local(),
+        #     self.uy.vector().get_local(),
+        #     ])
+        # jac = numpy.einsum('ikl,jl->jik', self.jacs, u)
+
+        jac_x = numpy.dot(self.jacs, self.ux.vector().get_local())
+        jac_y = numpy.dot(self.jacs, self.uy.vector().get_local())
+        jac = numpy.array([jac_x, jac_y])
 
         # jacs and J are of shape (2, 2, k). M must be of the same shape and
         # contain the result of the k 2x2 dot products. Perhaps there's a
         # dot() for this.
         M = numpy.einsum('ijl,jkl->ikl', jac, self.J)
+        # M = numpy.array([
+        #     [
+        #         jac[0][0]*self.J[0][0] + jac[0][1]*self.J[1][0],
+        #         jac[0][0]*self.J[0][1] + jac[0][1]*self.J[1][1],
+        #     ],
+        #     [
+        #         jac[1][0]*self.J[0][0] + jac[1][1]*self.J[1][0],
+        #         jac[1][0]*self.J[0][1] + jac[1][1]*self.J[1][1],
+        #     ],
+        #     ])
 
         # One could use
         #
@@ -393,19 +436,112 @@ class PiecewiseEllipse(object):
 
         q2, r2 = self.get_q2_r2()
 
-        out = len(q2) * numpy.array([
+        # scale the problem with 100 to avoid premature exit in optimization
+        out = 100 * numpy.array([
             res_x.get_local() / len(res_x.get_local()),
             res_y.get_local() / len(res_y.get_local()),
             (q2 - 1.0) / len(q2),
             r2 / len(r2),
             ])
 
-        if self.num_f_eval % 10 == 0:
+        if self.num_f_eval % 1000 == 0:
             cost = numpy.array([numpy.sum(ot**2) for ot in out])
             print('{:7d}     {:e} {:e} {:e} {:e}'.format(self.num_f_eval, *cost))
 
         self.num_f_eval += 1
         return numpy.concatenate(out)
+
+    def get_jac(self, alpha):
+        m = 2*self.V.dim() + 2*self.centers.shape[0]
+        n = alpha.shape[0]
+
+        ax, ay = numpy.split(alpha, 2)
+        jac_alpha_x = numpy.dot(self.jacs, ax)
+        jac_alpha_y = numpy.dot(self.jacs, ay)
+        jac_alpha = numpy.array([jac_alpha_x, jac_alpha_y])
+        M_alpha = numpy.einsum('ijl,jkl->ikl', jac_alpha, self.J)
+        #
+        a_alpha = (M_alpha[0, 0] + M_alpha[1, 1]) / 2
+        b_alpha = (M_alpha[0, 0] - M_alpha[1, 1]) / 2
+        c_alpha = (M_alpha[1, 0] + M_alpha[0, 1]) / 2
+        d_alpha = (M_alpha[1, 0] - M_alpha[0, 1]) / 2
+
+        # build lower q, r part of the linear operator
+        dq2 = []
+        dr2 = []
+        for k in range(n):
+            e = numpy.zeros(n)
+            e[k] = 1.0
+
+            ax, ay = numpy.split(e, 2)
+
+            # q2, r2 part
+            jac_phix = numpy.dot(self.jacs, ax)
+            jac_phiy = numpy.dot(self.jacs, ay)
+            jac_phi = numpy.array([jac_phix, jac_phiy])
+            M_phi = numpy.einsum('ijl,jkl->ikl', jac_phi, self.J)
+            #
+            a_phi = (M_phi[0, 0] + M_phi[1, 1]) / 2
+            b_phi = (M_phi[0, 0] - M_phi[1, 1]) / 2
+            c_phi = (M_phi[1, 0] + M_phi[0, 1]) / 2
+            d_phi = (M_phi[1, 0] - M_phi[0, 1]) / 2
+
+            dq2.append((a_alpha*a_phi + d_alpha*d_phi) / 2)
+            dr2.append((b_alpha*b_phi + c_alpha*c_phi) / 2)
+        dq2 = numpy.column_stack(dq2)
+        dr2 = numpy.column_stack(dr2)
+
+        def matvec(phi):
+            if len(phi.shape) > 1:
+                assert len(phi.shape) == 2
+                assert phi.shape[1] == 1
+                phi = phi[:, 0]
+
+            ax, ay = numpy.split(phi, 2)
+
+            # Laplace part (it's linear, so this is easy)
+            self.ux.vector().set_local(ax)
+            self.uy.vector().set_local(ay)
+            res_x = self.L * self.ux.vector()
+            res_y = self.L * self.uy.vector()
+
+            # q2, r2 part
+            dq2_phi = numpy.dot(dq2, phi)
+            dr2_phi = numpy.dot(dr2, phi)
+
+            # scale the problem with 100 to avoid premature exit in optimization
+            out = 100 * numpy.array([
+                res_x.get_local() / len(res_x.get_local()),
+                res_y.get_local() / len(res_y.get_local()),
+                dq2_phi / len(dq2),
+                dr2_phi / len(dr2),
+                ])
+            return numpy.concatenate(out)
+
+        def rmatvec(vec):
+            res_x, res_y, dq2_phi, dr2_phi = numpy.split(
+                vec,
+                numpy.cumsum([
+                    self.V.dim(), self.V.dim(), self.centers.shape[0]
+                    ])
+                )
+            res_x *= 100 / len(res_x)
+            res_y *= 100 / len(res_x)
+            dq2_phi *= 100 / len(dq2_phi)
+            dr2_phi *= 100 / len(dr2_phi)
+
+            self.ux.vector().set_local(res_x)
+            self.uy.vector().set_local(res_y)
+            phi = numpy.concatenate([
+                (self.L * self.ux.vector()).get_local(),
+                (self.L * self.uy.vector()).get_local(),
+                ])
+
+            q2p = numpy.dot(dq2.T, dq2_phi)
+            r2p = numpy.dot(dr2.T, dr2_phi)
+            return phi + q2p + r2p
+
+        return LinearOperator([m, n], matvec=matvec, rmatvec=rmatvec)
 
 
 def _main():
@@ -419,11 +555,19 @@ def _main():
 
     alpha0 = problem.alpha.copy()
 
+    numpy.random.seed(0)
+    alpha0 = numpy.random.rand(*alpha0.shape)
+
     # Levenberg-Marquardt (lm) is better suited for small, dense, unconstrained
     # problems, but it needs more conditions than parameters. This is not the
     # case for larger polynomial degrees.
     print('f evals     cost')
-    out = least_squares(problem.cost, alpha0, method='trf')
+    out = least_squares(
+        problem.cost, alpha0,
+        jac=problem.get_jac,
+        method='trf'
+        )
+    # out = minimize(problem.cost, alpha0, method='Powell')
 
     final_cost = numpy.sum(problem.cost(out.x)**2)
     print('{:7d}     {}'.format(problem.num_f_eval, final_cost))
@@ -445,31 +589,31 @@ def _main():
     #     plot_rgb_triangle=False,
     #     )
 
-    # # Plot perturbed MacAdam
-    # def transform(XY):
-    #     is_solo = len(XY.shape) == 1
-    #     if is_solo:
-    #         XY = numpy.array([XY]).T
-    #     # print(XY)
-    #     out = numpy.array([
-    #         [problem.ux(x, y) for x, y in XY.T],
-    #         [problem.uy(x, y) for x, y in XY.T],
-    #         ])
-    #     if is_solo:
-    #         out = out[..., 0]
-    #     return out
+    # Plot perturbed MacAdam
+    def transform(XY):
+        is_solo = len(XY.shape) == 1
+        if is_solo:
+            XY = numpy.array([XY]).T
+        # print(XY)
+        out = numpy.array([
+            [problem.ux(x, y) for x, y in XY.T],
+            [problem.uy(x, y) for x, y in XY.T],
+            ])
+        if is_solo:
+            out = out[..., 0]
+        return out
 
-    # plt.figure()
-    # # colorio.plot_luo_rigg(
-    # #     ellipse_scaling=1,
-    # colorio.plot_macadam(
-    #     ellipse_scaling=10,
-    #     # xy_to_2d=problem.pade2d.eval,
-    #     xy_to_2d=transform,
-    #     plot_rgb_triangle=False,
-    #     )
+    plt.figure()
+    # colorio.plot_luo_rigg(
+    #     ellipse_scaling=1,
+    colorio.plot_macadam(
+        ellipse_scaling=10,
+        # xy_to_2d=problem.pade2d.eval,
+        xy_to_2d=transform,
+        plot_rgb_triangle=False,
+        )
 
-    # plt.show()
+    plt.show()
     return
 
 
