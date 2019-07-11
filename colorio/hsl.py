5fa5132ea9f04dc7283eb1bd99fd32e5224be602
Nico Schlömer <nico.schloemer@gmail.com>
2019-07-11 15:07:31 +0200

    fix hsl

--- colorio/hsl.py
+++ colorio/hsl.py
@@ -4,6 +4,8 @@ import numpy
 class Hsl(object):
     def from_srgb1(self, srgb1):
         srgb = numpy.asarray(srgb1, dtype=float)
+        orig_shape = srgb.shape
+        srgb = srgb.reshape(3, -1)
         assert numpy.all(srgb >= 0)
         assert numpy.all(srgb <= 1)
 
@@ -13,7 +15,7 @@ class Hsl(object):
 
         diff = max_val - min_val
 
-        H = numpy.empty(srgb1.shape[1:], dtype=float)
+        H = numpy.empty(srgb.shape[1:], dtype=float)
         H[max_val == min_val] = 0
         i = argmax == 0
         H[i] = 60 * (0 + (srgb[1][i] - srgb[2][i]) / diff[i])
@@ -23,13 +25,16 @@ class Hsl(object):
         H[i] = 60 * (4 + (srgb[0][i] - srgb[1][i]) / diff[i])
         H = numpy.mod(H, 360)
 
-        S = numpy.empty(srgb1.shape[1:], dtype=float)
+        S = numpy.empty(srgb.shape[1:], dtype=float)
         S[max_val == 0] = 0
         S[min_val == 1] = 0
         S[(max_val > 0) & (min_val < 1)] = diff / (1 - numpy.abs(max_val + min_val - 1))
 
         L = (max_val + min_val) / 2
 
+        H = H.reshape(orig_shape[1:])
+        S = S.reshape(orig_shape[1:])
+        L = L.reshape(orig_shape[1:])
         return numpy.array([H, S, L])
 
     def to_srgb1(self, hsl):
@@ -44,19 +49,23 @@ class Hsl(object):
         C = (1 - numpy.abs(2 * L - 1)) * S
         H_dash = H / 60
         X = C * (1 - numpy.abs(numpy.mod(H_dash, 2) - 1))
-        if H_dash <= 1:
-            R1, G1, B1 = C, X, 0
-        elif H_dash <= 2:
-            R1, G1, B1 = X, C, 0
-        elif H_dash <= 3:
-            R1, G1, B1 = 0, C, X
-        elif H_dash <= 4:
-            R1, G1, B1 = 0, X, C
-        elif H_dash <= 5:
-            R1, G1, B1 = X, 0, C
-        else:
-            assert H_dash <= 6
-            R1, G1, B1 = C, 0, X
+        Z = numpy.zeros(C.shape)
+
+        R1 = numpy.empty(C.shape)
+        G1 = numpy.empty(C.shape)
+        B1 = numpy.empty(C.shape)
+        i = (0 < H_dash) & (H_dash <= 1)
+        R1[i], G1[i], B1[i] = C[i], X[i], Z[i]
+        i = (1 < H_dash) & (H_dash <= 2)
+        R1[i], G1[i], B1[i] = X[i], C[i], Z[i]
+        i = (2 < H_dash) & (H_dash <= 3)
+        R1[i], G1[i], B1[i] = Z[i], C[i], X[i]
+        i = (3 < H_dash) & (H_dash <= 4)
+        R1[i], G1[i], B1[i] = Z[i], X[i], C[i]
+        i = (4 < H_dash) & (H_dash <= 5)
+        R1[i], G1[i], B1[i] = X[i], Z[i], C[i]
+        i = (5 < H_dash) & (H_dash <= 6)
+        R1[i], G1[i], B1[i] = C[i], Z[i], X[i]
 
         m = L - C / 2
         return numpy.array([R1 + m, G1 + m, B1 + m])
