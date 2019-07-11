fd9092f290c4a336485e596aa5eb7303bba40da1
Nico Schlömer <nico.schloemer@gmail.com>
2019-07-11 14:30:19 +0200

    get hsl conversion right

--- colorio/hsl.py
+++ colorio/hsl.py
@@ -4,6 +4,8 @@ import numpy
 class Hsl(object):
     def from_srgb1(self, srgb1):
         srgb = numpy.asarray(srgb1, dtype=float)
+        assert numpy.all(srgb >= 0)
+        assert numpy.all(srgb <= 1)
 
         argmax = numpy.argmax(srgb)
         max_val = srgb[argmax]
@@ -29,19 +31,40 @@ class Hsl(object):
         if max_val == 0 or min_val == 1:
             S = 0
         else:
-            S = diff / (1 - numpy.abs(max_val + min_val - 1)) * 100
+            S = diff / (1 - numpy.abs(max_val + min_val - 1))
 
-        L = (max_val + min_val) / 2 * 100
+        L = (max_val + min_val) / 2
 
         return numpy.array([H, S, L])
 
-    def to_srgb1(self, srgb_linear):
-        a = 0.055
-        is_smaller = srgb_linear <= 0.0031308
-        srgb = numpy.array(srgb_linear, dtype=float)
-        srgb[is_smaller] *= 12.92
-        srgb[~is_smaller] = (1 + a) * srgb[~is_smaller] ** (1 / 2.4) - a
-        return srgb
+    def to_srgb1(self, hsl):
+        H, S, L = hsl
+        assert numpy.all(H >= 0)
+        assert numpy.all(H <= 360)
+        assert numpy.all(S >= 0)
+        assert numpy.all(S <= 1)
+        assert numpy.all(L >= 0)
+        assert numpy.all(L <= 1)
+
+        C = (1 - numpy.abs(2 * L - 1)) * S
+        H_dash = H / 60
+        X = C * (1 - numpy.abs(numpy.mod(H_dash, 2) - 1))
+        if H_dash <= 1:
+            R1, G1, B1 = C, X, 0
+        elif H_dash <= 2:
+            R1, G1, B1 = X, C, 0
+        elif H_dash <= 3:
+            R1, G1, B1 = 0, C, X
+        elif H_dash <= 4:
+            R1, G1, B1 = 0, X, C
+        elif H_dash <= 5:
+            R1, G1, B1 = X, 0, C
+        else:
+            assert H_dash <= 6
+            R1, G1, B1 = C, 0, X
+
+        m = L - C / 2
+        return numpy.array([R1 + m, G1 + m, B1 + m])
 
     def from_srgb256(self, srgb256):
         return self.from_srgb1(numpy.asarray(srgb256) / 255.0)
--- test/test_hsl.py
+++ test/test_hsl.py
@@ -3,22 +3,30 @@ import pytest
 
 import colorio
 
-# @pytest.mark.parametrize(
-#     "vals", [numpy.random.rand(3), numpy.random.rand(3, 7), numpy.random.rand(3, 4, 5)]
-# )
-# def test_conversion(vals):
-#     hsl = colorio.Hsl()
-#     out = hsl.to_srgb1(hsl.from_srgb1(vals))
-#     assert numpy.all(abs(vals - out) < 1.0e-14)
-#     return
+
+numpy.random.seed(2)
+
+
+@pytest.mark.parametrize(
+    "vals", [numpy.random.rand(3)]
+    # , numpy.random.rand(3, 7), numpy.random.rand(3, 4, 5)]
+)
+def test_conversion(vals):
+    hsl = colorio.Hsl()
+    print(vals)
+    print(hsl.from_srgb1(vals))
+    out = hsl.to_srgb1(hsl.from_srgb1(vals))
+    print(out)
+    assert numpy.all(abs(vals - out) < 1.0e-14)
+    return
 
 
 @pytest.mark.parametrize(
     "vals,ref",
     [
-        ([10, 20, 30], [210.0, 50.0, 400 / 51]),
-        ([110, 120, 130], [210.0, 25 / 3, 47.05882352941176]),
-        ([210, 220, 230], [210.0, 200 / 7, 4400 / 51]),
+        ([10, 20, 30], [210, 0.5, 4 / 51]),
+        ([110, 120, 130], [210, 25 / 300, 8 / 17]),
+        ([210, 220, 230], [210, 2 / 7, 44 / 51]),
     ],
 )
 def test_reference_srgb(vals, ref):
