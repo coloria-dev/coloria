0e2e3924552a8feca41b1d6a0695323128492cbf
Nico Schlömer <nico.schloemer@gmail.com>
2019-09-25 17:16:33 +0200

    be more consistent in color-constancy data

--- colorio/_color_space.py
+++ colorio/_color_space.py
@@ -470,7 +470,7 @@ class ColorSpace:
         with open(os.path.join(dir_path, "data/ebner_fairchild.yaml")) as f:
             data = yaml.safe_load(f)
 
-        wp = self.from_xyz100(numpy.array(data["white point"]))[1:]
+        wp = numpy.array(data["white point"])
 
         d = [
             numpy.column_stack([dat["reference xyz"], numpy.array(dat["same"]).T])
@@ -495,10 +495,8 @@ class ColorSpace:
         with open(os.path.join(dir_path, "data/hung-berns/table3.yaml")) as f:
             data = yaml.safe_load(f)
 
-        wp = self.from_xyz100(numpy.array(whitepoints_cie1931["C"]))[1:]
-
+        wp = numpy.array(whitepoints_cie1931["C"])
         d = [numpy.array(list(color.values())).T for color in data.values()]
-
         _plot_color_constancy_data(d, wp, self)
         return
 
@@ -583,9 +581,12 @@ class ColorSpace:
         return
 
 
-def _plot_color_constancy_data(data, wp, colorspace, approximate_colors_in_srgb=False):
+def _plot_color_constancy_data(
+    data_xyz100, wp_xyz100, colorspace, approximate_colors_in_srgb=False
+):
+    wp = colorspace.from_xyz100(wp_xyz100)[1:]
     srgb = SrgbLinear()
-    for xyz in data:
+    for xyz in data_xyz100:
         d = colorspace.from_xyz100(xyz)[1:]
 
         # There are numerous possibilities of defining the "best" approximating line for
@@ -620,6 +621,9 @@ def _plot_color_constancy_data(data, wp, colorspace, approximate_colors_in_srgb=
             ecol = srgb.to_srgb1(rgb) if is_legal_srgb else "black"
             plt.plot(dd[0], dd[1], "o", color=col, markeredgecolor=ecol)
 
+    plt.xlabel(colorspace.labels[1])
+    plt.ylabel(colorspace.labels[2])
+    plt.grid()
     plt.axis("equal")
     return
 
