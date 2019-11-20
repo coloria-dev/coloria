9e78c81b244d16ca6e9595b41bfe34a1b9a97489
Nico Schlömer <nico.schloemer@gmail.com>
2019-11-20 11:48:33 +0100

    osa.to_xyz: compute y0 with cardano, not newton

--- test/test_osa.py
+++ test/test_osa.py
@@ -19,8 +19,8 @@ numpy.random.seed(0)
 )
 def test_conversion(xyz):
     osa = colorio.OsaUcs()
-    out = osa.to_xyz100(osa.from_xyz100(xyz))
-    assert numpy.all(abs(xyz - out) < 1.0e-10)
+    out = osa.to_xyz100_newton(osa.from_xyz100(xyz))
+    assert numpy.all(abs(xyz - out) < 1.0e-9)
 
 
 def test_speed(N=2):
@@ -29,18 +29,24 @@ def test_speed(N=2):
     cielab = colorio.CIELAB()
     # cam16 = colorio.CAM16(0.69, 20, L_A=64 / numpy.pi / 5)
     ciecam02 = colorio.CIECAM02(0.69, 20, L_A=64 / numpy.pi / 5)
+
     perfplot.plot(
         # Don't use numpy.random.rand(3, n) to avoid the CIECAM breakdown
-        setup=lambda n: numpy.outer(numpy.random.rand(3), numpy.ones(n)) * 100,
+        setup=lambda n: numpy.outer(numpy.random.rand(3), numpy.ones(n)) * 10,
         equality_check=None,
         kernels=[
-            osa.from_xyz100,
-            cielab.from_xyz100,
-            # cam16.from_xyz100,
-            ciecam02.from_xyz100,
+            osa.to_xyz100,
+            cielab.to_xyz100,
+            # cam16.to_xyz100,
+            lambda Jsh: ciecam02.to_xyz100(Jsh, "Jsh"),
             numpy.cbrt,
         ],
-        labels=["OSA-UCS", "CIELAB", "CIECAM02", "cbrt"],
+        labels=[
+            "OSA-UCS",
+            "CIELAB",
+            "CIECAM02",
+            "cbrt"
+        ],
         n_range=[2 ** n for n in range(N)],
         logx=True,
         logy=True,
