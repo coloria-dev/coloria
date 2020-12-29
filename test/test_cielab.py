bb5fc90477adcfb2f14e2f9929a14400c5f65ec9
Nico Schlömer <nico.schloemer@gmail.com>
2020-12-29 15:15:26 +0100

    add more tests

--- test/test_cielab.py
+++ test/test_cielab.py
@@ -11,7 +11,6 @@ def test_conversion(xyz):
     cielab = colorio.CIELAB()
     out = cielab.to_xyz100(cielab.from_xyz100(xyz))
     assert numpy.all(abs(xyz - out) < 1.0e-14)
-    return
 
 
 @pytest.mark.parametrize(
@@ -19,6 +18,7 @@ def test_conversion(xyz):
     [
         ([10, 20, 30], [51.8372, -56.3591, -13.1812]),
         ([80, 90, 10], [95.9968, -10.6593, 102.8625]),
+        (colorio.illuminants.whitepoints_cie1931["D65"], [100.0, 0.0, 0.0]),
         # make sure we have a test point with values below the linearity point
         ([0.5, 0.6, 0.4], [5.4198, -2.8790, 3.6230]),
     ],
@@ -27,9 +27,8 @@ def test_reference_xyz(xyz100, ref):
     cielab = colorio.CIELAB()
     xyz100 = numpy.array(xyz100)
     assert numpy.all(
-        abs(cielab.from_xyz100(xyz100) - ref) < 1.0e-4 * abs(numpy.array(ref))
+        numpy.abs(cielab.from_xyz100(xyz100) - ref) < 1.0e-4 * numpy.abs(ref) + 1.0e-15
     )
-    return
 
 
 @pytest.mark.parametrize(
@@ -37,6 +36,7 @@ def test_reference_xyz(xyz100, ref):
     [
         ([2.61219, 1.52732, 10.96471], [12.7806, 26.1147, -52.4348]),
         ([2.39318, 2.01643, 1.64315], [15.5732, 9.7574, 0.2281]),
+        (colorio.illuminants.whitepoints_cie1931["D50"], [100.0, 0.0, 0.0]),
         # make sure we have a test point with values below the linearity point
         ([0.5, 0.6, 0.4], [5.4198, -3.1711, 1.7953]),
     ],
@@ -45,6 +45,5 @@ def test_reference_xyz_d50(xyz100, ref):
     cielab = colorio.CIELAB(whitepoint=colorio.illuminants.whitepoints_cie1931["D50"])
     xyz100 = numpy.array(xyz100)
     assert numpy.all(
-        abs(cielab.from_xyz100(xyz100) - ref) < 1.0e-4 * abs(numpy.array(ref))
+        numpy.abs(cielab.from_xyz100(xyz100) - ref) < 1.0e-4 * numpy.abs(ref) + 1.0e-15
     )
-    return
