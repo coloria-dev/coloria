45db964882db850e75d78e869e4b2eee154bdfd5
Nico Schlömer <nico.schloemer@gmail.com>
2021-01-06 11:35:29 +0100

    fewer data tests

--- test/data/test_ebner_fairchild.py
+++ test/data/test_ebner_fairchild.py
@@ -1,6 +1,3 @@
-import numpy
-import pytest
-
 import colorio
 
 
@@ -14,25 +11,9 @@ def test_show():
     # colorio.data.ebner_fairchild.savefig(cs)
 
 
-@pytest.mark.parametrize(
-    "cs,ref",
-    [
-        (colorio.CAM16UCS(0.69, 20, 64 / numpy.pi / 5), 3.7383772675948763),
-        (colorio.CAM02("UCS", 0.69, 20, 64 / numpy.pi / 5), 3.492838139468934),
-        (colorio.CIEHCL(), 12.619950376639341),
-        (colorio.CIELAB(), 4.095516235268518),
-        (colorio.CIELCH(), 13.405076662924223),
-        (colorio.CIELUV(), 3.7225918914653073),
-        (colorio.IPT(), 2.983536242312675),
-        (colorio.JzAzBz(), 3.1235467308302223),
-        (colorio.OKLAB(), 3.0983054621799875),
-        (colorio.OsaUcs(), 2.948843253469774),
-        (colorio.RLAB(), 4.1766551329386346),
-        (colorio.XYY(), 3.5552420588236453),
-    ],
-)
-def test_residuals(cs, ref):
-    print(cs)
+def test_residuals():
+    cs = colorio.CIELAB()
+    ref = 4.095516235268518
     res = sum(colorio.data.ebner_fairchild.residuals(cs))
     print(res)
     assert abs(res - ref) < 1.0e-14 * ref
--- test/data/test_xiao.py
+++ test/data/test_xiao.py
@@ -1,6 +1,3 @@
-import numpy
-import pytest
-
 import colorio
 
 
@@ -15,25 +12,9 @@ def test_show():
     # colorio.data.ebner_fairchild.savefig(cs)
 
 
-@pytest.mark.parametrize(
-    "cs,ref",
-    [
-        (colorio.CAM16UCS(0.69, 20, 64 / numpy.pi / 5), 0.7186726460882681),
-        (colorio.CAM02("UCS", 0.69, 20, 64 / numpy.pi / 5), 0.7799631516138833),
-        (colorio.CIEHCL(), 2.6848495918821063),
-        (colorio.CIELAB(), 0.5528109307168007),
-        (colorio.CIELCH(), 1.1028771830194923),
-        (colorio.CIELUV(), 0.6518104837712346),
-        (colorio.IPT(), 0.8928735078622902),
-        (colorio.JzAzBz(), 0.7538224281799465),
-        (colorio.OKLAB(), 0.758035735312263),
-        (colorio.OsaUcs(), 0.8347566618822679),
-        (colorio.RLAB(), 0.7089776800865354),
-        (colorio.XYY(), 0.4410546230668222),
-    ],
-)
-def test_residuals(cs, ref):
-    print(cs)
+def test_residuals():
+    cs = colorio.CIELAB()
+    ref = 0.5528109307168007
     res = sum(colorio.data.xiao.residuals(cs))
     print(res)
     assert abs(res - ref) < 1.0e-14 * ref
