0772f3b128fab96b92ce2c8d2559571bb7ec8b47
Nico Schlömer <nico.schloemer@gmail.com>
2021-01-06 12:46:48 +0100

    add missing files

---
+++ colorio/cs/__init__.py
@@ -0,0 +1,45 @@
+from ._cam16 import CAM16, CAM16UCS
+from ._ciecam02 import CAM02, CIECAM02, NegativeAError
+from ._ciehcl import CIEHCL
+from ._cielab import CIELAB
+from ._cielch import CIELCH
+from ._cieluv import CIELUV
+from ._color_space import ColorSpace
+from ._hdr import HdrLinear
+from ._hsl import HSL
+from ._hsv import HSV
+from ._ictcp import ICtCp
+from ._ipt import IPT
+from ._jzazbz import JzAzBz
+from ._oklab import OKLAB
+from ._osa import OsaUcs
+from ._rlab import RLAB
+from ._srgb import SrgbLinear
+from ._xyy import XYY
+from ._xyz import XYZ
+
+__all__ = [
+    "CAM16",
+    "CAM16UCS",
+    "CIECAM02",
+    "CAM02",
+    "NegativeAError",
+    "CIEHCL",
+    "CIELAB",
+    "CIELCH",
+    "CIELUV",
+    "ColorSpace",
+    "HSL",
+    "HSV",
+    "ICtCp",
+    "IPT",
+    "JzAzBz",
+    "observers",
+    "OKLAB",
+    "OsaUcs",
+    "HdrLinear",
+    "RLAB",
+    "SrgbLinear",
+    "XYY",
+    "XYZ",
+]
