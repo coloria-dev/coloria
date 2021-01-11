556016921b1f03e461f4558064fd33fcf588434f
Nico Schlömer <nico.schloemer@gmail.com>
2021-01-11 15:46:49 +0100

    make lint

--- test/data/test_macadam_1974.py
+++ test/data/test_macadam_1974.py
@@ -17,13 +17,13 @@ def test_show():
     # cs = colorio.cs.XYY1()
     colorio.data.macadam_1974.show(cs)
     with tempfile.TemporaryDirectory() as tmpdir:
-        colorio.data.macadam_1974.savefig(Path(tmpdir) / "out.png", cs, 50)
+        colorio.data.macadam_1974.savefig(Path(tmpdir) / "out.png", cs)
 
 
 def test_residual():
     cs = colorio.cs.CIELAB()
     ref = 0.2453191916738762
-    res = colorio.data.macadam_1974.residuals(cs)
+    res = colorio.data.macadam_1974.residual(cs)
     print(res)
     assert abs(res - ref) < 1.0e-14 * ref
 
