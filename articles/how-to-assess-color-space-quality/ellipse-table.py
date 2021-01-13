24ded1150801c28637f21b7adc2b9408e3941876
Nico Schlömer <nico.schloemer@gmail.com>
2021-01-14 00:48:21 +0100

    fix ellipse residuals

--- articles/how-to-assess-color-space-quality/ellipse-table.py
+++ articles/how-to-assess-color-space-quality/ellipse-table.py
@@ -3,22 +3,22 @@ import numpy
 import colorio
 
 color_spaces = [
-    colorio.CAM02("UCS", 0.69, 20, 64 / numpy.pi / 5),
-    colorio.CAM16UCS(0.69, 20, 64 / numpy.pi / 5),
-    colorio.CIELAB(),
-    colorio.CIELUV(),
-    colorio.IPT(),
-    colorio.JzAzBz(),
-    colorio.OKLAB(),
-    colorio.OsaUcs(),
-    colorio.RLAB(),
-    colorio.XYY(),
+    colorio.cs.CAM02("UCS", 0.69, 20, 64 / numpy.pi / 5),
+    colorio.cs.CAM16UCS(0.69, 20, 64 / numpy.pi / 5),
+    colorio.cs.CIELAB(),
+    colorio.cs.CIELUV(),
+    colorio.cs.IPT(),
+    colorio.cs.ICtCp(),
+    colorio.cs.JzAzBz(),
+    colorio.cs.OKLAB(),
+    colorio.cs.OsaUcs(),
+    colorio.cs.RLAB(),
+    colorio.cs.XYY1(),
 ]
 
 for cs in color_spaces:
     vals = [
-        colorio.data.macadam_1942.residuals(cs, 0.5),
-        colorio.data.luo_rigg.residuals(cs, 0.5),
+        colorio.data.macadam_1942.stress(cs, 0.5),
+        colorio.data.luo_rigg.stress(cs, 8),
     ]
-    s = sum(vals)
-    print(f"{cs.name} & {vals[0]:.3f} & {vals[1]:.3f} & {s:.3f}\\\\")
+    print(f"{cs.name} & {vals[0]:.1f} & {vals[1]:.1f}\\\\")
