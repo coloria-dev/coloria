6add25310f04ea13dde07e739026e8ad10ff009c
Nico Schlömer <nico.schloemer@gmail.com>
2021-01-16 00:41:27 +0100

    more invariance tests

--- test/data/test_invariance.py
+++ test/data/test_invariance.py
@@ -3,11 +3,26 @@ import pytest
 
 import colorio
 
+np.random.seed(0)
 
-class CielabTranslate():
+
+class CielabScaled():
     def __init__(self):
         self.cielab = colorio.cs.CIELAB()
-        self.x = np.array([0.1, 0.4, -1.1])
+        self.alpha = 2.41
+        self.k0 = self.cielab.k0
+
+    def from_xyz100(self, xyz100):
+        return self.cielab.from_xyz100(xyz100) * self.alpha
+
+    def to_xyz100(self, lab):
+        return self.cielab.to_xyz100(lab) / self.alpha
+
+
+class CielabTranslated():
+    def __init__(self):
+        self.cielab = colorio.cs.CIELAB()
+        self.x = np.random.rand(3)
         self.k0 = self.cielab.k0
 
     def from_xyz100(self, xyz100):
@@ -15,6 +30,20 @@ class CielabTranslate():
 
     def to_xyz100(self, xyz100):
         return self.cielab.to_xyz100(xyz100 - self.x)
+
+
+class CielabRotated():
+    def __init__(self):
+        self.cielab = colorio.cs.CIELAB()
+        self.R, _ = np.linalg.qr(np.random.rand(3, 3))
+        self.Rinv = np.linalg.inv(self.R)
+        self.k0 = self.cielab.k0
+
+    def from_xyz100(self, xyz100):
+        return self.R @ self.cielab.from_xyz100(xyz100)
+
+    def to_xyz100(self, lab):
+        return self.cielab.to_xyz100(self.Rinv @ lab)
 
 
 @pytest.mark.parametrize("fun", [
@@ -26,12 +55,13 @@ class CielabTranslate():
     colorio.data.witt.stress,
     colorio.data.xiao.stress,
 ])
-def test_invariance(fun):
+@pytest.mark.parametrize("ct", [CielabScaled(), CielabTranslated(), CielabRotated()])
+def test_invariance(fun, ct):
     cs = colorio.cs.CIELAB()
-    ct = CielabTranslate()
 
     val0 = fun(cs)
     val1 = fun(ct)
+    print(fun, ct)
     print(val0)
     print(val1)
     assert np.all(np.abs(val0 - val1) < 1.0e-14 * np.abs(val0))
