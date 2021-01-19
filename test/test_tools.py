8b3a0e100485032ccf02ec43a7f521a0b4bd7b6f
Nico Schlömer <nico.schloemer@gmail.com>
2021-01-19 16:24:16 +0100

    move test function

--- test/test_tools.py
+++ test/test_tools.py
@@ -10,15 +10,3 @@ def test_xy_gamut_mesh():
     colorio.xy_gamut_mesh(0.05)
     # import meshio
     # meshio.write_points_cells("test.vtu", points, {"triangle": cells})
-
-
-def test_srgb_gradient():
-    # cs = colorio.cs.CIELAB()
-    cs = colorio.cs.OKLAB()
-    # cs = colorio.cs.DIN99()
-    # cs = colorio.cs.CIELUV()
-    cs.show_srgb_gradient([0, 0, 255], [255, 255, 0])
-
-
-if __name__ == "__main__":
-    test_srgb_gradient()
