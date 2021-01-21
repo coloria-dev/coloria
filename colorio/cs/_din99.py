045125e23d5f69e22a1e664084337fc0bbbd02a9
Nico Schlömer <nico.schloemer@gmail.com>
2021-01-21 09:12:56 +0100

    small fixes for d99

--- colorio/cs/_din99.py
+++ colorio/cs/_din99.py
@@ -24,22 +24,24 @@ class DIN99(ColorSpace):
         if variant is None:
             self.p = [105.51, 0.0158, 16.0, 0.7, 200 / 9, 9 / 200, 0.0]
         elif variant == "b":
-            self.p = [303.67, 0.0039, 26.0, 0.83, 23.0, 0.075, 26 / 180 * np.pi]
+            self.p = [303.67, 0.0039, 26.0, 0.83, 23.0, 0.075, 26.0]
         elif variant == "c":
             self.p = [317.65, 0.0037, 0.0, 0.94, 23.0, 0.066, 0.0]
         else:
             assert variant == "d"
-            self.p = [325.22, 0.0036, 50.0, 1.14, 22.5, 0.06, 50 / 180 * np.pi]
+            self.p = [325.22, 0.0036, 50.0, 1.14, 22.5, 0.06, 50.0]
+
+        self.sin_p2 = np.sin(np.radians(self.p[2]))
+        self.cos_p2 = np.cos(np.radians(self.p[2]))
+        self.sin_p6 = np.sin(np.radians(self.p[6]))
+        self.cos_p6 = np.cos(np.radians(self.p[6]))
 
     def from_xyz100(self, xyz):
         L, a, b = self.cielab.from_xyz100(xyz)
         L99 = self.p[0] * np.log(1 + self.p[1] * L) / self.k_E
 
-        sin = np.sin(np.radians(self.p[2]))
-        cos = np.cos(np.radians(self.p[2]))
-
-        e = a * cos + b * sin
-        f = self.p[3] * (-a * sin + b * cos)
+        e = a * self.cos_p2 + b * self.sin_p2
+        f = self.p[3] * (-a * self.sin_p2 + b * self.cos_p2)
 
         G = np.hypot(e, f)
 
@@ -51,10 +53,8 @@ class DIN99(ColorSpace):
 
         cosarctan_f_e = e / G
         sinarctan_f_e = f / G
-        sin_p6 = np.sin(self.p[6])
-        cos_p6 = np.cos(self.p[6])
-        cos_h99 = cosarctan_f_e * cos_p6 - sinarctan_f_e * sin_p6
-        sin_h99 = sinarctan_f_e * cos_p6 + cosarctan_f_e * sin_p6
+        cos_h99 = self.cos_p6 * cosarctan_f_e - self.sin_p6 * sinarctan_f_e
+        sin_h99 = self.sin_p6 * cosarctan_f_e + self.cos_p6 * sinarctan_f_e
 
         a99 = C99 * cos_h99
         b99 = C99 * sin_h99
@@ -67,23 +67,24 @@ class DIN99(ColorSpace):
     def to_xyz100(self, lab99):
         L99, a99, b99 = lab99
         C99 = np.hypot(a99, b99)
-        G = (np.exp(0.045 * C99 * self.k_CH * self.k_E) - 1) / 0.045
+        G = (np.exp(C99 / self.p[4] * self.k_CH * self.k_E) - 1) / self.p[5]
 
-        hp = np.hypot(a99, b99)
-        cosarctan = a99 / hp
-        sinarctan = b99 / hp
-        cosarctan = np.nan_to_num(cosarctan, nan=0.0)
-        sinarctan = np.nan_to_num(sinarctan, nan=0.0)
+        cos_h99 = a99 / C99
+        sin_h99 = b99 / C99
 
-        e = G * cosarctan
-        f = G * sinarctan
+        cosarctan_f_e = self.cos_p6 * cos_h99 + self.sin_p6 * sin_h99
+        sinarctan_f_e = -self.sin_p6 * cos_h99 + self.cos_p6 * sin_h99
 
-        sin16 = np.sin(np.radians(16))
-        cos16 = np.cos(np.radians(16))
-        a = e * cos16 - f / 0.7 * sin16
-        b = e * sin16 + f / 0.7 * cos16
+        cosarctan_f_e = np.nan_to_num(cosarctan_f_e, nan=0.0)
+        sinarctan_f_e = np.nan_to_num(sinarctan_f_e, nan=0.0)
 
-        L = (np.exp(L99 * self.k_E / 105.51) - 1) / 0.0158
+        e = G * cosarctan_f_e
+        f = G * sinarctan_f_e
+
+        a = e * self.cos_p2 - f / self.p[3] * self.sin_p2
+        b = e * self.sin_p2 + f / self.p[3] * self.cos_p2
+
+        L = (np.exp(L99 * self.k_E / self.p[0]) - 1) / self.p[1]
 
         lab = np.array([L, a, b])
         return self.cielab.to_xyz100(lab)
