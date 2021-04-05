668520cf26674b973fc7786ec2519a7eae96fdf8
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-05 12:02:45 +0200

    more text on generalized means

--- articles/how-to-assess-color-space-quality/figures/figure-averages.py
+++ articles/how-to-assess-color-space-quality/figures/figure-averages.py
@@ -13,28 +13,36 @@ def pnorm(a, p):
     return (np.sum(a ** p) / n) ** (1 / p)
 
 
-np.random.seed(0)
-
 n = 20
 
-p = np.linspace(0.0, 5.0, 100)
+p = np.linspace(-5.0, 5.0, 101)
 # p = np.linspace(1.0, 10.0, 100)
 
 a = np.zeros(n)
 a[0] = 1.0
 y1 = [pnorm(a, P) for P in p]
 
+a = np.ones(n)
+a[1] = 1.0e-4
+y5 = [pnorm(a, P) for P in p]
+
 a = np.linspace(0.0, 1.0, n, endpoint=True)
+a[0] = 1.0e-4
 y2 = [pnorm(a, P) for P in p]
 
 a = np.full(n, 1.0)
 y3 = [pnorm(a, P) for P in p]
 
+a = np.full(n, 0.0)
+y6 = [pnorm(a, P) for P in p]
+
 plt.xlabel("$p$")
-plt.plot(p, y3, label="$s_{equal}$")
+plt.plot(p, y3, label="$s_{equal1}$")
+plt.plot(p, y5, label="$s_{outlier-small}$")
 plt.plot(p, y2, label="$s_{uniform}$")
-plt.plot(p, y1, label="$s_{outlier}$")
-plt.ylim(0.0, 1.1)
+plt.plot(p, y1, label="$s_{outlier1}$")
+plt.plot(p, y6, label="$s_{equal0}$")
+plt.ylim(-0.05, 1.1)
 # plt.gca().set_aspect("equal")
 
 # xticks = list(plt.xticks()[0])
@@ -47,5 +55,5 @@ plt.ylim(0.0, 1.1)
 plt.legend()
 # plt.show()
 tikzplotlib.save(
-    "norms.tex", extra_axis_parameters=["width=\\textwidth", "height=0.4\\textwidth"]
+    "averages.tex", extra_axis_parameters=["width=\\textwidth", "height=0.4\\textwidth"]
 )
