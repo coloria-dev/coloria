90d3d2fc4bf0a9537e4ac903fc6961a8850c1af8
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-16 15:51:12 +0200

    more whitepoint clarification

--- src/colorio/cs/_cieluv.py
+++ src/colorio/cs/_cieluv.py
@@ -1,15 +1,17 @@
 import numpy as np
+from numpy.typing import ArrayLike
 
 from ..illuminants import whitepoints_cie1931
 from ._color_space import ColorSpace
 
 
 class CIELUV(ColorSpace):
-    def __init__(self, whitepoint=whitepoints_cie1931["D65"]):
+    def __init__(self, whitepoint: ArrayLike = whitepoints_cie1931["D65"]):
         super().__init__("CIELUV", ("L*", "u*", "v*"), 0, is_origin_well_defined=False)
-        self.whitepoint = whitepoint
+        self.whitepoint_xyz100 = np.asarray(whitepoint)
+        self.whitepoint = np.array([100.0, 0.0, 0.0])
 
-    def from_xyz100(self, xyz):
+    def from_xyz100(self, xyz: ArrayLike) -> np.ndarray:
         def f(t):
             delta = 6 / 29
             out = np.array(t, dtype=float)
@@ -18,20 +20,21 @@ class CIELUV(ColorSpace):
             out[~is_greater] = out[~is_greater] / (delta / 2) ** 3
             return out
 
-        L = f(xyz[1] / self.whitepoint[1])
+        xyz = np.asarray(xyz)
+        L = f(xyz[1] / self.whitepoint_xyz100[1])
 
         x, y, z = xyz
         p = x + 15 * y + 3 * z
         u = 4 * x / p
         v = 9 * y / p
 
-        wx, wy, wz = self.whitepoint
+        wx, wy, wz = self.whitepoint_xyz100
         q = wx + 15 * wy + 3 * wz
         un = 4 * wx / q
         vn = 9 * wy / q
         return np.array([L, 13 * L * (u - un), 13 * L * (v - vn)])
 
-    def to_xyz100(self, luv):
+    def to_xyz100(self, luv: ArrayLike) -> np.ndarray:
         def f1(t):
             out = np.array(t, dtype=float)
             is_greater = out > 8
@@ -39,9 +42,9 @@ class CIELUV(ColorSpace):
             out[~is_greater] = out[~is_greater] * (3 / 29) ** 3
             return out
 
-        L, u, v = luv
+        L, u, v = np.asarray(luv)
 
-        wx, wy, wz = self.whitepoint
+        wx, wy, wz = self.whitepoint_xyz100
         q = wx + 15 * wy + 3 * wz
         un = 4 * wx / q
         vn = 9 * wy / q
