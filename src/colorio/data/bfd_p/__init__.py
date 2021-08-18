0ae7c430ddfc3f13bd99e6abd9049ffee60bacfd
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-18 17:31:15 +0200

    some comments

--- src/colorio/data/bfd_p/__init__.py
+++ src/colorio/data/bfd_p/__init__.py
@@ -1,3 +1,10 @@
+"""
+C. Alder, K.P. Chaing, T.F. Chong, E. Coates, A.A. Khalili and B. Rigg,
+Uniform Chromaticity Scales - New Experimental Data,
+Journal of The Society of Dyers and Colourists,
+Volume 98 January 1982,
+<https://doi.org/10.1111/J.1478-4408.1982.TB03602.X>
+"""
 import json
 import pathlib
 
@@ -10,5 +17,8 @@ class BfdP(ColorDistanceDataset):
 
         with open(this_dir / "bfd-p.json") as f:
             data = json.load(f)
+
+        # that's CIE 2-degree D64:
+        self.whitepoint_xyz100 = data["reference_white"]
 
         super().__init__("BFD-P", data["dv"], data["pairs"])
