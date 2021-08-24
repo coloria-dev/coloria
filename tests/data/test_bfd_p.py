26075c934312c5f2fc3158d33d4b2412aa9b29b1
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-25 00:25:14 +0200

    more tests

--- tests/data/test_bfd_p.py
+++ tests/data/test_bfd_p.py
@@ -1,3 +1,5 @@
+import pytest
+
 import colorio
 
 # def test_show():
@@ -16,18 +18,42 @@ import colorio
 #     plt.close()
 
 
-def test_stress():
-    cs = colorio.cs.CIELAB
+@pytest.mark.parametrize(
+    "cs_class,ref",
+    [
+        (colorio.cs.CAM16UCS, 30.912784895833845),
+        (colorio.cs.CAM02UCS, 30.96274433209427),
+        (colorio.cs.CAM02SCD, 32.74387216723911),
+        (colorio.cs.CAM02LCD, 33.69705929801087),
+        (colorio.cs.JzAzBz, 37.706002260925544),
+        (colorio.cs.OsaUcs, 40.16714621119245),
+        (colorio.cs.IPT, 41.08905311640498),
+        (colorio.cs.RLAB, 41.39045505197253),
+        (colorio.cs.CIELAB, 42.46259476788477),
+        (colorio.cs.SRLAB2, 42.68223438622064),
+        (colorio.cs.CIELUV, 43.41087692107525),
+        (colorio.cs.PROLAB, 44.40855824578013),
+        (colorio.cs.OKLAB, 47.77647147881965),
+        (colorio.cs.ICtCp, 55.60794680759048),
+        (colorio.cs.XYZ100, 69.58673571004842),
+        (colorio.cs.XYY100, 81.94385663616511),
+        (colorio.cs.CIELCH, 94.1656158806),
+        (colorio.cs.CIEHCL, 94.36424820618922),
+    ],
+)
+def test_stress(cs_class, ref):
+    print(cs_class)
     data = colorio.data.BfdP()
-    ref = 42.46259476788477
-    res = data.stress(cs)
+    res = data.stress(cs_class)
     print(res)
     assert abs(res - ref) < 1.0e-14 * ref
 
-    ref = 44.17345214550509
-    res = data.stress(cs, variant="relative")
-    print(res)
-    assert abs(res - ref) < 1.0e-14 * ref
+
+# TODO test relarive
+# ref = 44.17345214550509
+# res = data.stress(cs, variant="relative")
+# print(res)
+# assert abs(res - ref) < 1.0e-14 * ref
 
 
 if __name__ == "__main__":
