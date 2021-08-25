077ed003fb62bedbc3f74d12df1d5cbb247ea36f
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-25 14:20:46 +0200

    more test values

--- tests/data/test_ebner_fairchild.py
+++ tests/data/test_ebner_fairchild.py
@@ -12,32 +12,37 @@ def test_show():
 
 
 @pytest.mark.parametrize(
-    "cs_class,ref",
+    "cs_class,ref1,ref2",
     [
-        (colorio.cs.OsaUcs, 3.812972375907544),
-        (colorio.cs.IPT, 3.8585452834894975),
-        (colorio.cs.OKLAB, 4.057308865601472),
-        (colorio.cs.JzAzBz, 4.114638441215429),
-        (colorio.cs.SRLAB2, 4.472312427743778),
-        (colorio.cs.XYY100, 4.750458189192445),
-        (colorio.cs.CIELUV, 4.813605849721629),
-        (colorio.cs.CAM02LCD, 4.856397697283365),
-        (colorio.cs.PROLAB, 4.932645332164939),
-        (colorio.cs.CAM02UCS, 4.968202307667721),
-        (colorio.cs.CAM02SCD, 5.027025387481754),
-        (colorio.cs.CAM16UCS, 5.258885174710553),
-        (colorio.cs.CIELAB, 5.292068327281036),
-        (colorio.cs.RLAB, 5.3335671752151335),
-        (colorio.cs.ICtCp, 7.048597385158547),
-        # (colorio.cs.CIELCH, 11.89507120054368),
-        (colorio.cs.CIEHCL, 13.037122547036798),
+        (colorio.cs.OsaUcs, 3.812972375907544, 7.178611330824679),
+        (colorio.cs.IPT, 3.8585452834894975, 7.722339571500826),
+        (colorio.cs.OKLAB, 4.057308865601472, 7.161717923241752),
+        (colorio.cs.JzAzBz, 4.114638441215429, 8.80555808428739),
+        (colorio.cs.SRLAB2, 4.472312427743778, 8.872702833107542),
+        (colorio.cs.XYY100, 4.750458189192445, 13.435491691097537),
+        (colorio.cs.CIELUV, 4.813605849721629, 8.914484060462362),
+        (colorio.cs.CAM02LCD, 4.856397697283365, 8.050603730839033),
+        (colorio.cs.PROLAB, 4.932645332164939, 8.4138784020548),
+        (colorio.cs.CAM02UCS, 4.968202307667721, 8.220723083292194),
+        (colorio.cs.CAM02SCD, 5.027025387481754, 8.279318059320925),
+        (colorio.cs.CAM16UCS, 5.258885174710553, 8.660220687471277),
+        (colorio.cs.CIELAB, 5.292068327281036, 10.456987379313865),
+        (colorio.cs.RLAB, 5.3335671752151335, 9.965820911777676),
+        (colorio.cs.ICtCp, 7.048597385158547, 13.628048776065693),
+        # (colorio.cs.CIELCH, 11.89507120054368, 1.0),
+        (colorio.cs.CIEHCL, 13.037122547036798, 20.663099839073062),
     ],
 )
-def test_stress(cs_class, ref):
+def test_stress(cs_class, ref1, ref2):
     print(cs_class)
-    res = np.average(colorio.data.EbnerFairchild().stress(cs_class))
+    vals = colorio.data.EbnerFairchild().stress(cs_class)
+    res = np.average(vals)
     print(res)
-    assert abs(res - ref) < 1.0e-11 * ref
+    assert abs(res - ref1) < 1.0e-11 * ref1
+
+    res = np.max(vals)
+    print(res)
+    assert abs(res - ref2) < 1.0e-11 * ref2
 
 
 if __name__ == "__main__":
--- tests/data/test_xiao.py
+++ tests/data/test_xiao.py
@@ -12,32 +12,37 @@ def test_show():
 
 
 @pytest.mark.parametrize(
-    "cs_class,ref",
+    "cs_class,ref1,ref2",
     [
-        (colorio.cs.CIELUV, 2.381285478701682),
-        (colorio.cs.XYY100, 2.3985002202066097),
-        (colorio.cs.CIELAB, 2.788362604236149),
-        (colorio.cs.PROLAB, 2.8038926193818785),
-        (colorio.cs.SRLAB2, 3.1075993110078013),
-        (colorio.cs.RLAB, 3.6814827262133005),
-        (colorio.cs.ICtCp, 3.924094855240532),
-        (colorio.cs.CAM02SCD, 4.556735406166622),
-        (colorio.cs.CAM02UCS, 4.58825706817219),
-        (colorio.cs.CAM02LCD, 4.618341690395547),
-        (colorio.cs.CAM16UCS, 4.626957018528581),
-        (colorio.cs.JzAzBz, 5.230529469594494),
-        (colorio.cs.OKLAB, 6.256386444491906),
-        (colorio.cs.OsaUcs, 8.22761086694146),
-        (colorio.cs.IPT, 8.853878081896712),
-        # (colorio.cs.CIELCH, 9.983951212967135),
-        (colorio.cs.CIEHCL, 11.516088298979938),
+        (colorio.cs.CIELUV, 2.381285478701682, 3.1332518106519083),
+        (colorio.cs.XYY100, 2.3985002202066097, 3.4704952491749737),
+        (colorio.cs.CIELAB, 2.788362604236149, 5.414285724429276),
+        (colorio.cs.PROLAB, 2.8038926193818785, 4.372926666259824),
+        (colorio.cs.SRLAB2, 3.1075993110078013, 6.141156849717835),
+        (colorio.cs.RLAB, 3.6814827262133005, 5.822479304956172),
+        (colorio.cs.ICtCp, 3.924094855240532, 5.526529850337941),
+        (colorio.cs.CAM02SCD, 4.556735406166622, 7.79989808564784),
+        (colorio.cs.CAM02UCS, 4.58825706817219, 7.836629887470947),
+        (colorio.cs.CAM02LCD, 4.618341690395547, 7.839432495812633),
+        (colorio.cs.CAM16UCS, 4.626957018528581, 7.639901148051151),
+        (colorio.cs.JzAzBz, 5.230529469594494, 10.855292125203963),
+        (colorio.cs.OKLAB, 6.256386444491906, 9.59481014620974),
+        (colorio.cs.OsaUcs, 8.22761086694146, 12.136891378166084),
+        (colorio.cs.IPT, 8.853878081896712, 16.22085669819517),
+        # (colorio.cs.CIELCH, 9.983951212967135, 1.0),
+        (colorio.cs.CIEHCL, 11.516088298979938, 21.378029058252057),
     ],
 )
-def test_stress(cs_class, ref):
+def test_stress(cs_class, ref1, ref2):
     print(cs_class)
-    res = np.average(colorio.data.Xiao().stress(cs_class))
+    vals = colorio.data.Xiao().stress(cs_class)
+    res = np.average(vals)
     print(res)
-    assert abs(res - ref) < 1.0e-12 * ref
+    assert abs(res - ref1) < 1.0e-12 * ref1
+
+    res = np.max(vals)
+    print(res)
+    assert abs(res - ref2) < 1.0e-12 * ref2
 
 
 if __name__ == "__main__":
