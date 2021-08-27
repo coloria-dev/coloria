4428c81021fb9e734525819553c70b1081842a7e
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-27 15:20:47 +0200

    more von-kries tests

--- tests/cat/test_von_kries.py
+++ tests/cat/test_von_kries.py
@@ -1,20 +1,26 @@
 import numpy as np
+import pytest
 
 import colorio
 
 
-def test_von_kries():
-    xyz = [67.3, 20.3, 71.9]
-    whitepoint_test = colorio.illuminants.whitepoints_cie1931["D65"]
-    whitepoint_reference = colorio.illuminants.whitepoints_cie1964["C"]
-    ref = [68.367018961615, 20.3, 76.69540240441576]
-
-    cat = colorio.cat.VonKries(whitepoint_test, whitepoint_reference)
+@pytest.mark.parametrize(
+    "xyz,ref",
+    [
+        ([3.0, 4.0, 5.0], [3.1016365089661244, 4.0, 5.333477218665907]),
+        (
+            [67.3, 20.3, 71.9],
+            [68.367018961615, 20.3, 76.69540240441576],
+        ),
+        ([90.0, 92.0, 96.0], [91.9882823645901, 92.0, 102.40276259838542]),
+    ],
+)
+def test_von_kries(xyz, ref):
+    cat = colorio.cat.VonKries(
+        whitepoint_test=colorio.illuminants.whitepoints_cie1931["D65"],
+        whitepoint_reference=colorio.illuminants.whitepoints_cie1964["C"],
+    )
     out = cat.apply(xyz)
 
     print(list(out))
     assert np.all(np.abs(out - ref) < 1.0e-13 * out)
-
-    # check backwards tranformation
-    xyz2 = cat.apply_inv(out)
-    assert np.all(np.abs(xyz - xyz2) < 1.0e-13 * out)
