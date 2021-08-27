c58f6bad399ddc0109a281eaa746491cd664437c
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-27 10:04:21 +0200

    small optimization

--- src/colorio/cs/_cieluv.py
+++ src/colorio/cs/_cieluv.py
@@ -14,7 +14,10 @@ class CIELUV(ColorSpace):
 
     def __init__(self, whitepoint: ArrayLike = whitepoints_cie1931["D65"]):
         self.whitepoint_xyz100 = np.asarray(whitepoint)
-        self.whitepoint = np.array([100.0, 0.0, 0.0])
+        wx, wy, wz = self.whitepoint_xyz100
+        q = wx + 15 * wy + 3 * wz
+        self.un = 4 * wx / q
+        self.vn = 9 * wy / q
 
     def from_xyz100(self, xyz: ArrayLike) -> np.ndarray:
         xyz = np.asarray(xyz)
@@ -25,24 +28,15 @@ class CIELUV(ColorSpace):
         u = 4 * x / p
         v = 9 * y / p
 
-        wx, wy, wz = self.whitepoint_xyz100
-        q = wx + 15 * wy + 3 * wz
-        un = 4 * wx / q
-        vn = 9 * wy / q
-        return np.array([L, 13 * L * (u - un), 13 * L * (v - vn)])
+        return np.array([L, 13 * L * (u - self.un), 13 * L * (v - self.vn)])
 
     def to_xyz100(self, luv: ArrayLike) -> np.ndarray:
         L, u, v = np.asarray(luv)
 
-        wx, wy, wz = self.whitepoint_xyz100
-        q = wx + 15 * wy + 3 * wz
-        un = 4 * wx / q
-        vn = 9 * wy / q
+        uu = u / (13 * L) + self.un
+        vv = v / (13 * L) + self.vn
 
-        uu = u / (13 * L) + un
-        vv = v / (13 * L) + vn
-
-        Y = wy * finv(L)
+        Y = self.whitepoint_xyz100[1] * finv(L)
         X = Y * 9 * uu / (4 * vv)
         Z = Y * (12 - 3 * uu - 20 * vv) / (4 * vv)
         return np.array([X, Y, Z])
