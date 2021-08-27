a84b53819c1742b13a04f14e5e1f964fa698065a
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-27 18:25:18 +0200

    simplify von-kries

--- src/colorio/cat/von_kries.py
+++ src/colorio/cat/von_kries.py
@@ -11,34 +11,32 @@ from numpy.typing import ArrayLike
 
 
 class VonKries:
-    def __init__(
-        self,
-        whitepoint_test: ArrayLike,
-        whitepoint_reference: ArrayLike,
-        exact_inversion: bool = True,
-    ):
-        self.judd = np.array(
+    def __init__(self, whitepoint_test: ArrayLike, whitepoint_reference: ArrayLike):
+        judd = np.array(
             [
                 [0.0, 1.0, 0.0],
                 [-0.46, 1.36, 0.1],
                 [0.0, 0.0, 1.0],
             ]
         )
-        judd_inv_approx = np.array(
-            [
-                [2.954, -2.174, 0.22],
-                [1.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0],
-            ]
-        )
-        self.judd_inv = np.linalg.inv(self.judd) if exact_inversion else judd_inv_approx
+        # judd_inv_approx = np.array(
+        #     [
+        #         [2.954, -2.174, 0.22],
+        #         [1.0, 0.0, 0.0],
+        #         [0.0, 0.0, 1.0],
+        #     ]
+        # )
+        # judd_inv = np.linalg.inv(judd) if exact_inversion else judd_inv_approx
 
-        rgb_rw = self.judd @ whitepoint_reference
-        rgb_w = self.judd @ whitepoint_test
-        self.abc = rgb_rw / rgb_w
+        rgb_rw = judd @ whitepoint_reference
+        rgb_w = judd @ whitepoint_test
+        abc = rgb_rw / rgb_w
+
+        self.M = np.linalg.solve(judd, (judd.T * abc).T)
+        self.Minv = np.linalg.inv(self.M)
 
     def apply(self, xyz: ArrayLike) -> np.ndarray:
-        return npx.dot(self.judd_inv, (npx.dot(self.judd, xyz).T * self.abc).T)
+        return npx.dot(self.M, xyz)
 
     def apply_inv(self, xyz: ArrayLike) -> np.ndarray:
-        return npx.dot(self.judd_inv, (npx.dot(self.judd, xyz).T / self.abc).T)
+        return npx.dot(self.Minv, xyz)
