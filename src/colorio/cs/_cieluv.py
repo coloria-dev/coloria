f6ccf04e92999f68f73a53ab2b0c81a1396bffa8
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-27 10:01:15 +0200

    deduplicate code

--- src/colorio/cs/_cieluv.py
+++ src/colorio/cs/_cieluv.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy.typing import ArrayLike
 
 from ..illuminants import whitepoints_cie1931
+from ._cielab import f, finv
 from ._color_space import ColorSpace
 
 
@@ -16,14 +17,6 @@ class CIELUV(ColorSpace):
         self.whitepoint = np.array([100.0, 0.0, 0.0])
 
     def from_xyz100(self, xyz: ArrayLike) -> np.ndarray:
-        def f(t):
-            delta = 6 / 29
-            out = np.array(t, dtype=float)
-            is_greater = out > delta ** 3
-            out[is_greater] = 116 * np.cbrt(out[is_greater]) - 16
-            out[~is_greater] = out[~is_greater] / (delta / 2) ** 3
-            return out
-
         xyz = np.asarray(xyz)
         L = f(xyz[1] / self.whitepoint_xyz100[1])
 
@@ -39,13 +32,6 @@ class CIELUV(ColorSpace):
         return np.array([L, 13 * L * (u - un), 13 * L * (v - vn)])
 
     def to_xyz100(self, luv: ArrayLike) -> np.ndarray:
-        def f1(t):
-            out = np.array(t, dtype=float)
-            is_greater = out > 8
-            out[is_greater] = ((out[is_greater] + 16) / 116) ** 3
-            out[~is_greater] = out[~is_greater] * (3 / 29) ** 3
-            return out
-
         L, u, v = np.asarray(luv)
 
         wx, wy, wz = self.whitepoint_xyz100
@@ -56,7 +42,7 @@ class CIELUV(ColorSpace):
         uu = u / (13 * L) + un
         vv = v / (13 * L) + vn
 
-        Y = wy * f1(L)
+        Y = wy * finv(L)
         X = Y * 9 * uu / (4 * vv)
         Z = Y * (12 - 3 * uu - 20 * vv) / (4 * vv)
         return np.array([X, Y, Z])
