6fc9de83752a7df5f73f3f2a348eb41b54565b2e
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-30 23:01:56 +0200

    add bianco-schettini CAT

--- src/colorio/cat/__init__.py
+++ src/colorio/cat/__init__.py
@@ -1,3 +1,4 @@
+from .bianci_schettini import bianci_schettini, bianci_schettini_pos
 from .bradford import bradford
 from .cat02 import cat02
 from .cat16 import cat16
@@ -5,4 +6,13 @@ from .cmccat2000 import cmccat2000
 from .sharp import sharp
 from .von_kries import von_kries
 
-__all__ = ["bradford", "cat02", "cat16", "cmccat2000", "sharp", "von_kries"]
+__all__ = [
+    "bianci_schettini",
+    "bianci_schettini_pos",
+    "bradford",
+    "cat02",
+    "cat16",
+    "cmccat2000",
+    "sharp",
+    "von_kries",
+]
