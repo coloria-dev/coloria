4744fb54131bbcb532f270d4086614abf224f1d9
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-30 10:50:33 +0200

    remove cmccat97

--- src/colorio/cat/__init__.py
+++ src/colorio/cat/__init__.py
@@ -1,7 +1,6 @@
 from .cat02 import CAT02
 from .cat16 import CAT16
-from .cmccat97 import CMCCAT97
 from .cmccat2000 import CMCCAT2000
 from .von_kries import VonKries
 
-__all__ = ["VonKries", "CMCCAT97", "CMCCAT2000", "CAT02", "CAT16"]
+__all__ = ["VonKries", "CMCCAT2000", "CAT02", "CAT16"]
